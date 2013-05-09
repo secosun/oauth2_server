ce2b100af85e113ebcf459af3fd4cc301c7ba2c6
Bojan Zivanovic <bojanz@gmail.com>
2013-05-09 14:58:42 +0200

    Update for library changes (oauth2-server-php v0.7).

--- lib/Scope.php
+++ lib/Scope.php
@@ -15,7 +15,7 @@ class OAuth2_Scope_Drupal implements OAuth2_ScopeInterface
    * Check if everything in required scope is contained in available scope.
    *
    * @param $required_scope
-   *   Required scope to be check with.
+   * A space-separated string of scopes.
    *
    * @return
    * TRUE if everything in required scope is contained in available scope,
@@ -23,45 +23,45 @@ class OAuth2_Scope_Drupal implements OAuth2_ScopeInterface
    *
    * @see http://tools.ietf.org/html/rfc6749#section-7
    *
-   * @ingroup oauth2_server_section_7
+   * @ingroup oauth2_section_7
    */
-  public function checkScope($required_scope, $available_scope) {
-    // The required scope should match or be a subset of the available scope
-    if (!is_array($required_scope)) {
-      $required_scope = explode(' ', trim($required_scope));
-    }
+  function checkScope($required_scope, $available_scope) {
+    $required_scope = explode(' ', trim($required_scope));
+    $available_scope = explode(' ', trim($available_scope));
+    return (count(array_diff($required_scope, $available_scope)) == 0);
+  }
 
-    if ($available_scope == '*') {
-      // Get all scope entities that match the provided scope.
-      // Compare the difference.
-      $query = new EntityFieldQuery();
-      $query->entityCondition('entity_type', 'oauth2_server_scope');
-      $query->propertyCondition('server', $this->server->name);
-      $query->propertyCondition('name', $required_scope);
-      $query->addTag('oauth2_server_scope_access');
-      $query->addMetaData('oauth2_server', $this->server);
-      $results = $query->execute();
-      if ($results) {
-        $scope_ids = array_keys($results['oauth2_server_scope']);
-        $scopes = entity_load('oauth2_server_scope', $scope_ids);
-        $found_scope = array();
-        foreach ($scopes as $scope) {
-          $found_scope[] = $scope->name;
-        }
+  /**
+   * Check if the provided scope exists in storage.
+   *
+   * @param $scope
+   *   A space-separated string of scopes.
+   * @param $client_id
+   *   The requesting client.
+   *
+   * @return
+   *   TRUE if it exists, FALSE otherwise.
+   */
+  function scopeExists($scope, $client_id = null) {
+    $scope = explode(' ', trim($scope));
+    // Get all scope entities that match the provided scope.
+    // Compare the difference.
+    $query = new EntityFieldQuery();
+    $query->entityCondition('entity_type', 'oauth2_server_scope');
+    $query->propertyCondition('server', $this->server->name);
+    $query->propertyCondition('name', $scope);
+    $query->addTag('oauth2_server_scope_access');
+    $query->addMetaData('oauth2_server', $this->server);
+    $results = $query->execute();
+    if ($results) {
+      $scope_ids = array_keys($results['oauth2_server_scope']);
+      $loaded_scopes = entity_load('oauth2_server_scope', $scope_ids);
+      $found_scope = array();
+      foreach ($loaded_scopes as $loaded_scope) {
+        $found_scope[] = $loaded_scope->name;
+      }
 
-        return (count(array_diff($required_scope, $found_scope)) == 0);
-      }
-      else {
-        // No matching scopes found.
-        return FALSE;
-      }
-    }
-    else {
-      if (!is_array($available_scope)) {
-        $available_scope = explode(' ', trim($available_scope));
-      }
-
-      return (count(array_diff($required_scope, $available_scope)) == 0);
+      return (count(array_diff($scope, $found_scope)) == 0);
     }
   }
 
@@ -89,12 +89,5 @@ class OAuth2_Scope_Drupal implements OAuth2_ScopeInterface
     }
 
     return FALSE;
-  }
-
-  public function getSupportedScopes($client_key = null) {
-    // The OAuth2 module is designed for an unbounded number of scopes, so it
-    // is not feasible to return them all. Instead, we return a magic value
-    // that tells checkScope() to query against the database.
-    return '*';
   }
 }
